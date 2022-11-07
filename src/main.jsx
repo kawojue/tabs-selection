227db4632ed93cb6bb9b321080e43f0a576fa8bb
Raheem Kawojue <kawojue08@gmail.com>
2022-11-07 20:46:38 +0100

    setup context

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
+import './index.css'
+import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { DataProvider } from './components/Context'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <DataProvider>
+      <App />
+    </DataProvider>
   </React.StrictMode>
 )
