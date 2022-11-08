08ff39454d5397f56b8f37df631f0a12f0df0bb5
Raheem Kawojue <kawojue08@gmail.com>
2022-11-08 22:38:03 +0100

    used components

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,30 +1,11 @@
-import Context from './Context'
+import Job from './Job'
 import Buttons from './Buttons'
-import { useContext } from 'react'
 
 const Menu = () => {
-    const { jobs, value, FaAngleDoubleRight } = useContext(Context)
-
-    const { title, company, dates, duties } = jobs[value]
-
     return (
         <section className="menu">
             <Buttons />
-            <article className="">
-                <div>
-                    <h3>{title}</h3>
-                    <h5>{company}</h5>
-                    <p>{dates}</p>
-                </div>
-                <ul>
-                    {duties.map((duty, index) => (
-                        <li key={index}>
-                            <FaAngleDoubleRight />
-                            {duty}
-                        </li>
-                    ))}
-                </ul>
-            </article>
+            <Job />
         </section>
     )
 }
