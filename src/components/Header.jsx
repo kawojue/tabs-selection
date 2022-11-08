40c90eb32f186d0064e169771e19abed10e00f65
Raheem Kawojue <kawojue08@gmail.com>
2022-11-08 13:03:58 +0100

    created Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+
+    return (
+        <header>
+            <h1 className="info">Experience</h1>
+            <div></div>
+        </header>
+    )
+}
+
+export default Header
