1859dfe6f8a98ab961e08fc59657bd109e44e069
Raheem Kawojue <kawojue08@gmail.com>
2022-11-08 09:12:34 +0100

    fetched jobs

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -6,9 +6,36 @@ const Context = createContext({})
 
 export const DataProvider = ({ children }) => {
     const url = 'https://course-api.com/react-tabs-project'
+    const [isLoading, setIsLoading] = useState(true)
+    const [fetchErr, setFetchErr] = useState(null)
+    const [jobs, setJobs] = useState([])
+
+    const fetchJobs = async () => {
+        setIsLoading(true)
+        setFetchErr(null)
+        try {
+            const res = await fetch(url)
+            if (!res.ok) throw new Error("Please, check your connection.")
+            const data = await res.json()
+            setJobs(data)
+        } catch {
+            setFetchErr("Please, reload the page.")
+        }
+    }
+
+    useEffect(() => {
+        (async () => await fetchJobs())()
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 1200)
+    }, [])
+
+    console.log(jobs)
 
     return (
-        <Context.Provider value={{}}>
+        <Context.Provider value={{
+            isLoading, fetchErr, jobs
+        }}>
             {children}
         </Context.Provider>
     )
